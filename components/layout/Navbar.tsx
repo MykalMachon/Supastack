4ca159e7f19ce730a3703564641553b271943b11
Mykal Machon <mykalmachon@gmail.com>
2021-03-24 14:00:42 -0700

    feat: styled basic navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,35 +1,40 @@
 import useAuth from 'hooks/useAuth';
+import styles from './Navbar.module.scss';
 import Link from 'next/link';
 
 const Navbar = () => {
   const { auth } = useAuth();
 
   return (
-    <nav className="navbar">
-      <strong>Supastack</strong>
-      {auth.user() ? (
-        <>
-          <p>
-            {auth.user().email}
-            <button
-              onClick={() => {
-                auth.signOut();
-              }}
-            >
-              Signout
-            </button>
-          </p>
-        </>
-      ) : (
-        <>
-          <Link href="/signin">
-            <a>Signin</a>
-          </Link>
-          <Link href="/signin">
-            <a>Signup</a>
-          </Link>
-        </>
-      )}
+    <nav className={styles.navbar}>
+      <div className={styles['nav-content']}>
+        <Link href="/">
+          <a className={styles.logo}>Supastack</a>
+        </Link>
+        {auth.user() ? (
+          <>
+            <div className={styles['nav-content__actions']}>
+              <span>{auth.user().email}</span>
+              <button
+                onClick={() => {
+                  auth.signOut();
+                }}
+              >
+                Signout
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className={styles['nav-content__actions']}>
+            <Link href="/signin">
+              <a>Signin</a>
+            </Link>
+            <Link href="/signup">
+              <a>Signup</a>
+            </Link>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
