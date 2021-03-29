cd0cf59984802c67cfcd96844536be6067fa7346
Mykal Machon <mykal.machon@student.ufv.ca>
2021-03-28 23:35:41 -0700

    fix: fixing icons & adding pencil icon

--- components/icons/PencilIcon.tsx
+++ components/icons/PencilIcon.tsx
@@ -1,21 +1,21 @@
 import { IconProps } from '@utils/types';
 
-const LikeIcon = ({ active = false }: IconProps) => {
+const PencilIcon = ({ active }: IconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke={active ? 'var(--highlightBg)' : 'var(--bodyText)'}
+      stroke="currentColor"
     >
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="2"
-        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+        d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
       />
     </svg>
   );
 };
 
-export default LikeIcon;
+export default PencilIcon;
